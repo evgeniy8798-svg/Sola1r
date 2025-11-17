8e720f95bee7ae4e45b4e09f83b0c8b33a50964e
evgeniy8798-svg <evgeniy8798@gmail.com>
2025-11-17 10:15:30 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Це перевірник паролю!!!")
+
+password = 12345
+
+num1 = int(input ("Веди пароль:"))
+
+if  password == num1:
+    
+    print("ТАк пароль вірний!")
+else :  password <= num1
+print("Пароль не вірний !")
+
+mainloop:()
