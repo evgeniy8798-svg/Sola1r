e981e88b0156b5392859c269ff2d18e1d8ebf4ac
evgeniy8798-svg <evgeniy8798@gmail.com>
2025-11-30 18:34:57 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-print("Це перевірник паролю!!!")
+Записна_книжка_телефона = {
+    "FIRSTabonement": "",
+    "SECONDabonent": "",
+    "THIRDabonent": ""
+}
+print(Записна_книжка_телефона)
+print("-" * 90)
 
-password = 12345
+name1 = input ("Веди ім'я")
+num1 = input ("Види номер телефона")
 
-num1 = int(input ("Веди пароль:"))
+name2 = input ("Веди ім'я")
+num2 = input ("Види номер телефона")
 
-if  password == num1:
-    
-    print("ТАк пароль вірний!")
-else :  password <= num1
-print("Пароль не вірний !")
+name3 = input ("Веди ім'я")
+num3 = input ("Види номер телефона")
 
-mainloop:()
+Записна_книжка_телефона ["FIRSTabonement"] = "" + num1
+
+Записна_книжка_телефона  ["SECONDabonent"] = "" + num2
+
+Записна_книжка_телефона  ["THIRDabonent"] = "" + num3
+
+FOURobonenet = input ("Види ім'я")
+num4 = input ("Види номер телефона" )
+
+Записна_книжка_телефона [ "FOURabonent"  ] = num4
+
+print(Записна_книжка_телефона)
+print("-" * 90)
